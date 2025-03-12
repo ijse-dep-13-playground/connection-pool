5cbfdc3090928a10fa588e02302e8de205daaddc
kushansm <kushandileep12@mail.com>
2025-03-12 13:25:09 +0530

    add logic to find the request id and rest and setup respond for release and obtain

--- src/main/java/lk/ijse/dep13/connectionpool/ReleaseConnectionServlet.java
+++ src/main/java/lk/ijse/dep13/connectionpool/ReleaseConnectionServlet.java
@@ -7,11 +7,34 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(name = "release-connection-servlet", urlPatterns = "/connections/*")
 public class ReleaseConnectionServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("ObtainConnectionServlet");
+        if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
+            releaseAllConnections(resp);
+        }else{
+            Matcher matcher = Pattern.compile("^/(?<id>[^/]+)/?(?<rest>.*)$").matcher(req.getPathInfo());
+            if (matcher.find()){
+                String id = matcher.group("id");
+                String rest = matcher.group("rest");
+                if (rest.isBlank()){
+                    releaseConnection(id, resp);
+                }else{
+                    resp.sendError(404);
+                }
+            }
+        }
+    }
+    private void releaseConnection(String id, HttpServletResponse resp) throws IOException {
+        System.out.println("ReleaseConnectionServlet");
+    }
+
+    private void releaseAllConnections(HttpServletResponse resp) throws IOException {
+        System.out.println("ReleaseAllConnectionServlet");
     }
 }
