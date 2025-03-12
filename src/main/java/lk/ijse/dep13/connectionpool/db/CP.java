e225824a8f112c1aa1c6e4286afa61da98f7028e
kushansm <kushandileep12@mail.com>
2025-03-12 13:35:33 +0530

    add UUID genarator for key

--- src/main/java/lk/ijse/dep13/connectionpool/db/CP.java
+++ src/main/java/lk/ijse/dep13/connectionpool/db/CP.java
@@ -6,12 +6,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Properties;
+import java.util.UUID;
 
 public class CP {
     private static final int DEFAULT_POOL_SIZE = 5;
 
-    private final HashMap<Integer, Connection> MAIN_POOL = new HashMap<>();
-    private final HashMap<Integer, Connection> CONSUMER_POOL = new HashMap<>();
+    private final HashMap<UUID, Connection> MAIN_POOL = new HashMap<>();
+    private final HashMap<UUID, Connection> CONSUMER_POOL = new HashMap<>();
 
     private  int poolSize;
 
@@ -40,12 +41,42 @@ public class CP {
         String user = properties.getProperty("app.db.user");
         String password = properties.getProperty("app.db.password");
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
 
         for (int i = 0; i < poolSize; i++) {
             Connection connection = DriverManager.getConnection("jdbc:mysql://%s:%s/%s"
                     .formatted(host, port, database), user, password);
-            MAIN_POOL.put((i + 1) * 10, connection);
+            UUID id = UUID.randomUUID();
+            MAIN_POOL.put(id, connection);
         }
+    }
+    public synchronized ConnectionWrapper getConnection() {
+        while (MAIN_POOL.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        UUID key = MAIN_POOL.keySet().stream().findFirst().get();
+        Connection connection = MAIN_POOL.get(key);
+        MAIN_POOL.remove(key);
+        CONSUMER_POOL.put(key, connection);
+        return new ConnectionWrapper(key, connection);
+    }
+
+    public synchronized void releaseConnection(UUID id){
+        if (!CONSUMER_POOL.containsKey(id)) throw new RuntimeException("Invalid Connection ID");
+        Connection connection = CONSUMER_POOL.get(id);
+        CONSUMER_POOL.remove(id);
+        MAIN_POOL.put(id, connection);
+        notify();
+    }
+
+    public synchronized void releaseAllConnections(){
+        CONSUMER_POOL.forEach(MAIN_POOL::put);
+        CONSUMER_POOL.clear();
+        notifyAll();
+    }
+    public record ConnectionWrapper(UUID id, Connection connection) {
     }
 }
