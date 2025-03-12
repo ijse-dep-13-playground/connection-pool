3ef7f87407dd4c53d49e4e2875fe95f5bd03d5f7
kushansm <kushandileep12@mail.com>
2025-03-12 13:41:37 +0530

    add containerservletinitializer

--- src/main/java/lk/ijse/dep13/connectionpool/listener/AppListener.java
+++ src/main/java/lk/ijse/dep13/connectionpool/listener/AppListener.java
@@ -1,4 +1,14 @@
 package lk.ijse.dep13.connectionpool.listener;
 
-public class AppListener {
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+import java.util.Set;
+
+public class AppListener implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
+
+    }
 }
