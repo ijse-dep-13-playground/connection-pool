899ce8d2781e7b982d7818e7647ae52256d82181
kushansm <kushandileep12@mail.com>
2025-03-12 13:43:19 +0530

    add AppListner

--- src/main/java/lk/ijse/dep13/connectionpool/listener/AppListener.java
+++ src/main/java/lk/ijse/dep13/connectionpool/listener/AppListener.java
@@ -3,12 +3,14 @@ package lk.ijse.dep13.connectionpool.listener;
 import jakarta.servlet.ServletContainerInitializer;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import lk.ijse.dep13.connectionpool.db.CP;
 
 import java.util.Set;
 
 public class AppListener implements ServletContainerInitializer {
     @Override
     public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
-
+        CP cp = new CP();
+        servletContext.setAttribute("datasource", cp);
     }
 }
